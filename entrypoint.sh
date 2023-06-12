dd06684efae2b6812bbbc5c8969db6d2d564e403
Soph <consts7s@gmail.com>
2023-06-12 05:20:33 -0400

    Updated gunicorn timeout to stop issues when updating database with api

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "start" ]; then
     flask db upgrade
-    gunicorn --bind 0.0.0.0:5000 wsgi:app
+    gunicorn --bind 0.0.0.0:5000 --timeout 600 wsgi:app
 elif [ "$1" == "shell" ]; then
     /bin/bash
 else
