5b86133f29e2397d20d6cdaf82f6bba207958d64
Soph <consts7s@gmail.com>
2023-06-16 07:13:05 -0400

    Added cron service start to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ "$1" == "start" ]; then
+    printenv >> /etc/environment
+    service cron start
     flask db upgrade
     gunicorn --bind 0.0.0.0:5000 --timeout 600 --workers="2" -- wsgi:app
 elif [ "$1" == "shell" ]; then
