bf97e73e1a771f3f8d04ffd34fa8af08e5408724
Soph <consts7s@gmail.com>
2023-06-16 14:47:02 -0400

    Added slippi.js to make requests to the slippi.gg GraphQL api

---
+++ static/js/slippi.js
@@ -0,0 +1,73 @@
+function fetchAccountData(cc, uid) {
+  // GraphQL query
+  const query = `
+    query AccountManagementPageQuery($cc: String!, $uid: String!) {
+      getUser(fbUid: $uid) {
+        ...userProfilePage
+        __typename
+      }
+      getConnectCode(code: $cc) {
+        user {
+          ...userProfilePage
+          __typename
+        }
+        __typename
+      }
+    }
+    
+    fragment userProfilePage on User {
+      fbUid
+      displayName
+      connectCode {
+        code
+        __typename
+      }
+      rankedNetplayProfile {
+        id
+        ratingOrdinal
+        ratingUpdateCount
+        wins
+        losses
+        dailyGlobalPlacement
+        dailyRegionalPlacement
+        continent
+        characters {
+          id
+          character
+          gameCount
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }
+  `;
+
+  // Variables for the query
+  const variables = {
+    cc: cc,
+    uid: uid,
+  };
+
+  // Make the GraphQL request
+  fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Extract the relevant values from the response
+      const user = data.data.getConnectCode.user;
+
+      return user
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
